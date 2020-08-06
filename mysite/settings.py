486b5c8f8d3b308da894dd520b8772dc5f53e614
Manolis Korniotakis <manolis@buster.unisystems.gr>
2020-08-06 17:38:57 +0300

    Deploy app to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, '/bootstrap-theme/assets/img/')
 
